b51898f140474bbe9ac9201974fdfc790fa54b95
seonew <seonew.888@gmail.com>
2018-04-27 01:46:35 +0900

    Add `index.html`

---
+++ src/components/Rating.js
@@ -0,0 +1,29 @@
+Vue.component('coco-rating', {
+  template: 
+    '<div>\
+      <div style="font-size:1.5em; color:#e64980">\
+        <span class="fa-stack">\
+          <i class="fas fa-star fa-stack-1x"></i>\
+          <i class="far fa-star fa-stack-1x"></i>\
+        </span>\
+        <span class="fa-stack">\
+          <i class="fas fa-star fa-stack-1x"></i>\
+          <i class="far fa-star fa-stack-1x"></i>\
+        </span>\
+        <span class="fa-stack">\
+          <i class="fas fa-star fa-stack-1x"></i>\
+          <i class="far fa-star fa-stack-1x"></i>\
+        </span>\
+        <span class="fa-stack">\
+          <i class="fas fa-star-half fa-stack-1x"></i>\
+          <i class="far fa-star fa-stack-1x"></i>\
+        </span>\
+        <span class="fa-stack">\
+          <i class="far fa-star fa-stack-1x"></i>\
+          <i class="far fa-star fa-stack-1x"></i>\
+        </span>\
+      </div>\
+    </div>',
+    methods: {
+    }
+})
